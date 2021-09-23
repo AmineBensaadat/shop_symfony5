a433f14a7c289cb431f715d86319d5a06aa76a2c
amine bensaadat <bensaadat.amine.@gmail.com>
2021-09-23 17:37:16 +0100

    44. Tunnel d'achat : Choix de l'adresse de livraison

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\OrderType;
+
+class OrderController extends AbstractController
+{
+    /**
+     * @Route("/order", name="order")
+     */
+    public function index(): Response
+    {
+        if(!$this->getUser()->getAddresses()){
+            return $this->redirectToRoute('account_address_add');
+        }
+        $form = $this->createForm(OrderType::class, null, [
+            'user' =>$this->getUser()
+        ]);
+        return $this->render('order/index.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+}
